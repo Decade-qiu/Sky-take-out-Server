d9f27bc8c21f9879509f68c6ecdbd1a597f4b415
Decade-qiu <3237550600@qq.com>
2024-04-14 22:08:12 +0800

    :sparkles:add employee module

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,21 +1,26 @@
 package com.sky.controller.admin;
 
+import com.github.pagehelper.Page;
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,12 +29,53 @@ import java.util.Map;
 @RestController
 @RequestMapping("/admin/employee")
 @Slf4j
+@Api(tags = "员工")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
     @Autowired
     private JwtProperties jwtProperties;
+
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result<String> save(@RequestBody EmployeeDTO employee) {
+        employeeService.save(employee);
+        return Result.success();
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("分页查询员工")
+    public Result page(EmployeePageQueryDTO employeePageQueryDTO) {
+        Integer page = employeePageQueryDTO.getPage();
+        Integer pageSize = employeePageQueryDTO.getPageSize();
+        String keyword = employeePageQueryDTO.getName();
+        PageResult employees = employeeService.pageQuery(page, pageSize, keyword);
+
+        return Result.success(employees);
+    }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("设置员工状态")
+    public Result setStatus(@PathVariable Integer status, @RequestParam Long id){
+        employeeService.setStatus(status, id);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工")
+    public Result<Employee> getById(@PathVariable Long id) {
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    @PutMapping()
+    @ApiOperation("修改员工")
+    public Result<String> update(@RequestBody EmployeeDTO employeeDTO) {
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
+
 
     /**
      * 登录
